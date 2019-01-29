37fb878b70a7ed0690ce327d2e3f28241b5bdd79
ZosiaStafford <46976875+ZosiaStafford@users.noreply.github.com>
2019-01-29 00:14:20 -0500

    Update hw2_functions.py

--- hw2_functions.py
+++ hw2_functions.py
@@ -45,9 +45,9 @@ def intersectionoftwolines_y(m1, b1, m2, b2):
 def distancebetweenpoints(x1, y1, x2, y2):
     # Calculate the linear distance between two points
     # (x1, y1) and (x2, y2).
+    # d = sqrt((x2-x1)+(y2-y1))
 
-
-    distance = 0 # replace with your calculation for distance
+    distance = math.sqrt(((x2-x1)**2)+((y2-y1)**2)) # replace with your calculation for distance
     return distance
 
 def heronsformula(a, b, c):
