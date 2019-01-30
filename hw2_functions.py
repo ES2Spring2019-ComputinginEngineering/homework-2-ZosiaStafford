7066776ee84b7411072b7794270130ac696975cf
ZosiaStafford <46976875+ZosiaStafford@users.noreply.github.com>
2019-01-30 16:49:35 -0500

    Update hw2_functions.py

--- hw2_functions.py
+++ hw2_functions.py
@@ -24,6 +24,7 @@
 import math #This line allows you to use math functions. Importantly, math.sqrt(#) which will produce the square root of the number inside the parentheses.
 
 
+
 def intersectionoftwolines_x(m1, b1, m2, b2):
     # Calculate x for the point where two equations:
     # y = (m1 * x) + b1 and y = (m2 * x) + b2 intersect.
@@ -32,16 +33,15 @@ def intersectionoftwolines_x(m1, b1, m2, b2):
     #(m1-m2)*x = b2-b1
     #x = (b2-b1)/(m1-m2)
 
-    x = (b2-b1)/(m1-m2) #replace this with your calculation for x
+    x = (b2-b1)/(m1-m2) #calculation for x
     return x
 
 def intersectionoftwolines_y(m1, b1, m2, b2):
     # Calculate y for the point where two equations:
     # y = (m1 * x) + b1 and y = (m2 * x) + b2 intersect.
 
-    y = (m1*((b2-b1)/(m1-m2)))+b1 #replace this with your calculation for y
+    y = (m1*((b2-b1)/(m1-m2)))+b1 #calculation for y
     return y
-
 
 def distancebetweenpoints(x1, y1, x2, y2):
     # Calculate the linear distance between two points
@@ -64,20 +64,32 @@ def areaofatriangle(m1, b1, m2, b2, m3, b3):
     #Using the three functions above, now calculate the area of a
     #triangle when the three sides are described by three linear equations
     #y = (m1 * x) + b1;  y = (m2 * x) + b2; and y = (m3 * x) + b3
-    x1= intersectionoftwolines_x(m1, b1, m2, b2)
-    x2=intersectionoftwolines_x(m2, b2, m3, b3)
-    x3=intersectionoftwolines_x(m3, b3, m1, b1)
+    if (m1 == m2) or (m2 == m3) or (m1 == m3):
+        #The slopes are equal, so there is no triangle
+        print("Error = There are parallel lines")
+        return None
 
-    y1=intersectionoftwolines_y(m1, b1, m2, b2)
-    y2=intersectionoftwolines_y(m2, b2, m3, b3)
-    y3=intersectionoftwolines_y(m3, b3, m1, b1)
+    else:
+        x1= intersectionoftwolines_x(m1, b1, m2, b2)
+        x2=intersectionoftwolines_x(m2, b2, m3, b3)
+        x3=intersectionoftwolines_x(m3, b3, m1, b1)
 
-    a=distancebetweenpoints(x1, y1, x2, y2)
-    b=distancebetweenpoints(x2, y2, x3, y3)
-    c=distancebetweenpoints(x3, y3, x1, y1)
+        y1=intersectionoftwolines_y(m1, b1, m2, b2)
+        y2=intersectionoftwolines_y(m2, b2, m3, b3)
+        y3=intersectionoftwolines_y(m3, b3, m1, b1)
 
-    area = heronsformula(a, b, c) #replace this with your calculation for area
-    return area
+        a=distancebetweenpoints(x1, y1, x2, y2)
+        b=distancebetweenpoints(x2, y2, x3, y3)
+        c=distancebetweenpoints(x3, y3, x1, y1)
+
+        if (a==0) or (b==0) or (c==0):
+            print("Error = Lines intersect at same point")
+            return None
+
+        else:
+            area = heronsformula(a, b, c) #replace this with your calculation for area
+            return area
+
 
 
 #TEST CASES
@@ -106,4 +118,14 @@ print("Area of a Triangle:")
 #If these are both true, it is likely that your function is working.
 print(round(areaofatriangle(10, 10, 20, 0, 30, 0),2) == 2.5)
 print(round(areaofatriangle(0, 0, 1, 0, -1, 10),2) == 25)
+print("*********")
+
+print("Parallel Line Test:")
+#If this is true, it is likely that your function is working.
+print(areaofatriangle(2, 10, 2, 5, 30, 0))
+print("*********")
+
+print("Lines All Intersect Test:")
+#If this is true, it is likely that your function is working.
+print(areaofatriangle(2, 1, 1, 1, 5, 1))
 print("*********")
